20a690be6ec29acfdbe4f2330f136d7c5e6e42ec
Deivid Moura <deividmourans@hotmail.com>
2019-10-06 17:59:04 -0300

    Status das Bases

--- ConsultasUteis/src/SQLServer/Consultas/Status das Bases.sql
+++ ConsultasUteis/src/SQLServer/Consultas/Status das Bases.sql
@@ -1,88 +1,88 @@
 SELECT
-    CONVERT(VARCHAR(25), DB.name) AS dbName,
-    state_desc,
+    CONVERT(VARCHAR(25), DB.NAME) AS DBNAME,
+    STATE_DESC,
     (
         SELECT
             COUNT(1)
         FROM
-            sys.master_files
+            SYS.MASTER_FILES
         WHERE
-            DB_NAME(database_id) = DB.name
-            AND type_desc = 'rows'
-    ) AS DataFiles,
+            DB_NAME(DATABASE_ID) = DB.NAME
+            AND TYPE_DESC = 'ROWS'
+    ) AS DATAFILES,
     (
         SELECT
-            SUM(( size * 8 ) / 1024)
+            SUM(( SIZE * 8 ) / 1024)
         FROM
-            sys.master_files
+            SYS.MASTER_FILES
         WHERE
-            DB_NAME(database_id) = DB.name
-            AND type_desc = 'rows'
-    ) AS [Data MB],
+            DB_NAME(DATABASE_ID) = DB.NAME
+            AND TYPE_DESC = 'ROWS'
+    ) AS [DATA MB],
     (
         SELECT
             COUNT(1)
         FROM
-            sys.master_files
+            SYS.MASTER_FILES
         WHERE
-            DB_NAME(database_id) = DB.name
-            AND type_desc = 'log'
-    ) AS LogFiles,
+            DB_NAME(DATABASE_ID) = DB.NAME
+            AND TYPE_DESC = 'LOG'
+    ) AS LOGFILES,
     (
         SELECT
-            SUM(( size * 8 ) / 1024)
+            SUM(( SIZE * 8 ) / 1024)
         FROM
-            sys.master_files
+            SYS.MASTER_FILES
         WHERE
-            DB_NAME(database_id) = DB.name
-            AND type_desc = 'log'
-    ) AS [Log MB],
-    recovery_model_desc AS [Recovery model],
-    CASE [compatibility_level]
-        WHEN 60 THEN '60 (SQL Server 6.0)'
-        WHEN 65 THEN '65 (SQL Server 6.5)'
-        WHEN 70 THEN '70 (SQL Server 7.0)'
-        WHEN 80 THEN '80 (SQL Server 2000)'
-        WHEN 90 THEN '90 (SQL Server 2005)'
-        WHEN 100 THEN '100 (SQL Server 2008)'
-        WHEN 110 THEN '110 (SQL Server 2012)'
-        WHEN 120 THEN '120 (SQL Server 2014)'
-        WHEN 130 THEN '130 (SQL Server 2016)'
-        WHEN 140 THEN '140 (SQL Server 2017)'
-        WHEN 150 THEN '150 (SQL Server 2019)'
-    END AS [compatibility level],
-    CONVERT(VARCHAR(20), create_date, 103) + ' ' + CONVERT(VARCHAR(20), create_date, 108) AS [Creation date],
-    -- last backup
+            DB_NAME(DATABASE_ID) = DB.NAME
+            AND TYPE_DESC = 'LOG'
+    ) AS [LOG MB],
+    RECOVERY_MODEL_DESC AS [RECOVERY MODEL],
+    CASE [COMPATIBILITY_LEVEL]
+        WHEN 60 THEN '60 (SQL SERVER 6.0)'
+        WHEN 65 THEN '65 (SQL SERVER 6.5)'
+        WHEN 70 THEN '70 (SQL SERVER 7.0)'
+        WHEN 80 THEN '80 (SQL SERVER 2000)'
+        WHEN 90 THEN '90 (SQL SERVER 2005)'
+        WHEN 100 THEN '100 (SQL SERVER 2008)'
+        WHEN 110 THEN '110 (SQL SERVER 2012)'
+        WHEN 120 THEN '120 (SQL SERVER 2014)'
+        WHEN 130 THEN '130 (SQL SERVER 2016)'
+        WHEN 140 THEN '140 (SQL SERVER 2017)'
+        WHEN 150 THEN '150 (SQL SERVER 2019)'
+    END AS [COMPATIBILITY LEVEL],
+    CONVERT(VARCHAR(20), CREATE_DATE, 103) + ' ' + CONVERT(VARCHAR(20), CREATE_DATE, 108) AS [CREATION DATE],
+    -- LAST BACKUP
     ISNULL(
     (
         SELECT TOP 1
-            CASE type WHEN 'D' THEN 'Full' WHEN 'I' THEN 'Differential' WHEN 'L' THEN 'Transaction log' END + ' – ' + LTRIM(ISNULL(STR(ABS(DATEDIFF(DAY, GETDATE(), backup_finish_date))) + ' days ago', 'NEVER')) + ' – ' + CONVERT(VARCHAR(20), backup_start_date, 103) + ' ' + CONVERT(VARCHAR(20), backup_start_date, 108) + ' – ' + CONVERT(VARCHAR(20), backup_finish_date, 103) + ' ' + CONVERT(VARCHAR(20), backup_finish_date, 108) + ' (' + CAST(DATEDIFF(SECOND, BK.backup_start_date, BK.backup_finish_date) AS VARCHAR(4)) + ' ' + 'seconds)'
+            CASE TYPE WHEN 'D' THEN 'FULL' WHEN 'I' THEN 'DIFFERENTIAL' WHEN 'L' THEN 'TRANSACTION LOG' END + ' – ' + LTRIM(ISNULL(STR(ABS(DATEDIFF(DAY, GETDATE(), BACKUP_FINISH_DATE))) + ' DAYS AGO', 'NEVER')) + ' – ' + CONVERT(VARCHAR(20), BACKUP_START_DATE, 103) + ' ' + CONVERT(VARCHAR(20), BACKUP_START_DATE, 108) + ' – ' + CONVERT(VARCHAR(20), BACKUP_FINISH_DATE, 103) + ' ' + CONVERT(VARCHAR(20), BACKUP_FINISH_DATE, 108) + ' (' + CAST(DATEDIFF(SECOND, BK.BACKUP_START_DATE, BK.BACKUP_FINISH_DATE) AS VARCHAR(4)) + ' ' + 'SECONDS)'
         FROM
-            msdb..backupset BK
+            MSDB..BACKUPSET BK
         WHERE
-            BK.database_name = DB.name
+            BK.DATABASE_NAME = DB.NAME
         ORDER BY
-            backup_set_id DESC
+            BACKUP_SET_ID DESC
     ),    '-'
-          ) AS [Last backup],
-    CASE WHEN is_auto_close_on = 1 THEN 'autoclose' ELSE '' END AS [autoclose],
-    page_verify_option_desc AS [page verify option],
-    CASE WHEN is_auto_shrink_on = 1 THEN 'autoshrink' ELSE '' END AS [autoshrink],
-    CASE WHEN is_auto_create_stats_on = 1 THEN 'auto create statistics' ELSE '' END AS [auto create statistics],
-    CASE WHEN is_auto_update_stats_on = 1 THEN 'auto update statistics' ELSE '' END AS [auto update statistics],
-    DB.delayed_durability_desc,
-    DB.is_parameterization_forced,
-    DB.user_access_desc,
-    DB.snapshot_isolation_state_desc,
-    DB.is_read_only,
-    DB.is_trustworthy_on,
-    DB.is_encrypted,
-    DB.is_query_store_on,
-    DB.is_cdc_enabled,
-    DB.is_remote_data_archive_enabled,
-    DB.is_subscribed,
-    DB.is_merge_published
+          ) AS [LAST BACKUP],
+    CASE WHEN IS_AUTO_CLOSE_ON = 1 THEN 'AUTOCLOSE' ELSE '' END AS [AUTOCLOSE],
+    PAGE_VERIFY_OPTION_DESC AS [PAGE VERIFY OPTION],
+    CASE WHEN IS_AUTO_SHRINK_ON = 1 THEN 'AUTOSHRINK' ELSE '' END AS [AUTOSHRINK],
+    CASE WHEN IS_AUTO_CREATE_STATS_ON = 1 THEN 'AUTO CREATE STATISTICS' ELSE '' END AS [AUTO CREATE STATISTICS],
+    CASE WHEN IS_AUTO_UPDATE_STATS_ON = 1 THEN 'AUTO UPDATE STATISTICS' ELSE '' END AS [AUTO UPDATE STATISTICS],
+    DB.DELAYED_DURABILITY_DESC,
+    DB.IS_PARAMETERIZATION_FORCED,
+    DB.USER_ACCESS_DESC,
+    DB.SNAPSHOT_ISOLATION_STATE_DESC,
+    DB.IS_READ_ONLY,
+    DB.IS_TRUSTWORTHY_ON,
+    DB.IS_ENCRYPTED,
+    DB.IS_QUERY_STORE_ON,
+    DB.IS_CDC_ENABLED,
+    DB.IS_REMOTE_DATA_ARCHIVE_ENABLED,
+    DB.IS_SUBSCRIBED,
+    DB.IS_MERGE_PUBLISHED
 FROM
-    sys.databases DB
+    SYS.DATABASES DB
 ORDER BY
     6 DESC;
